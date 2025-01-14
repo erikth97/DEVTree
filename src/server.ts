525b5bff44388370a6043792c0c474bc36b8d5a5
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-01-13 23:48:25 -0600

    Feat: Add Mongo DB instance

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'  // ESM Ecmascript modules
 import router from './router';
+import { connectDB } from './config/db';
 
 const app = express()
+
+connectDB()
 
 // READ DATA FROM FORMS
 app.use(express.json())
