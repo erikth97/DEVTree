43967d91340a9b274db317dcbb1a840ac462f334
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-01-15 13:33:31 -0600

    Feat: Add return msg response

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express"
+import User from "./models/User"
+
 const router = Router ()
 
 // A U T H  & R E G I S T E R 
-router.post('/auth/register', (req, res) => {
-    console.log(req.body)
+router.post('/auth/register', async (req, res) => {
+    const user = new User(req.body)
+    
+    await user.save()
+
+    res.send('User Registered')
 })
 
 export default router;
