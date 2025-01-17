7f81d80dba6061978ae265eb368cd95fb6cf0d04
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-01-17 14:04:08 -0600

    Feat: Add Hash Password

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (req: Request, res: Response) => {
 
-    const {email} = req.body
+    const {email, password} = req.body
 
     const userExists = await User.findOne({email})
     if (userExists) {
@@ -11,10 +12,8 @@ export const createAccount = async (req: Request, res: Response) => {
         return res.status(409).json({error : error.message})
     } 
 
-
     const user = new User(req.body)
-    
+    user.password = await hashPassword(password)
     await user.save()
-
     res.status(201).send('User Registered')
 }
