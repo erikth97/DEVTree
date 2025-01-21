4f12d74cdb049104590df06f237beb71c886768a
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-01-20 23:30:37 -0600

    Feat: Add Validation

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,17 @@
 import type { Request, Response } from "express"
+import { validationResult } from "express-validator"
 import slug from "slug"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    // E R R O R  H A N D L I N G
+    let errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})   
+    }
+    return
 
     const { email, password } = req.body
 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import { Router} from "express";
+import { body } from "express-validator"; 
 import { createAccount } from "./handlers";
 
 const router = Router()
 
 // A U T H  &  R E G I S T R A T I O N
-router.post('/register', createAccount)
+router.post('/auth/register', 
+    body('handle').notEmpty(),
+    createAccount)
 
-export default router 
+export default router
